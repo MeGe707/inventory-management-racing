2b1b56824aebf6bd34f08899a0c642273b951f1e
Mehmet Gül <mehmet0707gul@gmail.com>
2025-12-06 02:08:31 +0300

    yoyoyo

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,16 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://inventory-management-racing-j2he.onrender.com",
+    ],
+    credentials: true,
+  })
+);
+
 
 
 app.use("/user", userRouter);
